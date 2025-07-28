19189c80918e2b1824b7eba2fc39858d69bbf5f8
vijay416 <102639639+vijay416@users.noreply.github.com>
2025-07-28 15:59:00 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'pages/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Med Report Scanner',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: HomePage(),
+    );
+  }
+}
